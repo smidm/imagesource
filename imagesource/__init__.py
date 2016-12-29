9a075fc0c6f0926076ec97a9e805298be6e78671
Matěj Šmíd <smidm@cmp.felk.cvut.cz>
2016-12-29 16:53:19 +0100

    static image source

--- imagesource/__init__.py
+++ imagesource/__init__.py
@@ -3,6 +3,7 @@ from .files import FilesSource
 from .video import VideoSource
 from .timedvideo import TimedVideoSource
 from .synchronized import SynchronizedSource
+from .static import StaticSource
 
-__all__ = [ImageSource, FilesSource, VideoSource, TimedVideoSource, SynchronizedSource]
+__all__ = [ImageSource, FilesSource, VideoSource, TimedVideoSource, SynchronizedSource, StaticSource]
 
---
+++ imagesource/static.py
@@ -0,0 +1,25 @@
+from .base import ImageSource
+import cv2
+
+
+class StaticSource(ImageSource):
+    def __init__(self, file_name, mask=None):
+        super(StaticSource, self).__init__(mask)
+        self.file_name = file_name
+        self.img = cv2.imread(self.file_name)
+
+    def get_image(self, frame):
+        if self.color_conversion_from_bgr is not None:
+            img = cv2.cvtColor(self.img, self.color_conversion_from_bgr)
+        else:
+            img = self.img
+        return self.mask_image(img)
+
+    def get_next_image(self):
+        return self.get_image(0)
+
+    def rewind(self):
+        pass
+
+
+
